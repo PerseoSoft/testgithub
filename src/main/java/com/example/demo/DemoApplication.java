c6792490bfcbd4f17719bd52134b4cca38a98a42
Eva Villarreal Guzmán <villarrealguzman@gmail.com>
2024-04-02 22:00:45 -0300

    adfasdf

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,6 +7,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		String regexPattern = "tra(vel|ce|de)m";
+		Pattern pattern = Pattern.compile(regexPattern);
+		for(String value : values) {
+			Matcher matcher = pattern.matcher(value);
+			if (matcher.matches()) {
+				// do something
+			}
+		}
+
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
